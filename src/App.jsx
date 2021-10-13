370e2d7c90a86a50d2b36267db47891f01ba5f34
João Victor <soulima.joao@gmail.com>
2021-10-13 12:00:18 -0300

    estilização da exibição das palavras digitadas corretamente" dasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
-
-const Wrapper = styled.div`
-`
+import Rubik from './fonts/Rubik-VariableFont_wght.ttf'
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: "Rubik";
+        src: url(${Rubik});
+    }
+
     * {
         margin: 0;
         padding: 0;
@@ -19,15 +22,69 @@ const GlobalStyle = createGlobalStyle`
         display: flex;
         justify-content: center;
         align-items: center;
+        font-family: 'Rubik', sans-serif;
+    }
+`
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .valid-keys {
+        font-size: 10em;
+        font-weight: bold;
+        min-width: 600px;
+        border-bottom: solid 2px #f1f1f1;
+
+        .matched {
+            background-color: #fdf8c5;
+            color: #282c24
+        }
+
+        .remainder {
+            opacity: 0.5;
+        }
+    }
+
+    .completed-words {
+        width: max-content;
+        height: 100vh;
+        overflow-y: scroll;
+        position: fixed;
+        top: 0;
+        right: 0;
+        text-align: left;
+        padding: 20px 40px;
+        font-size: 1.8em;
+        line-height: 2.5rem;
+        opacity: 0.5;
     }
 `
 
 const App = (props) => {
     return (
-        <Wrapper>
+        <React.Fragment>
             <GlobalStyle />
-            <h1>Hello World</h1>
-        </Wrapper>
+            <Wrapper>
+                <div className="valid-keys">
+                    <span className="matched">hellowor</span>
+                    <span className="remainder">ld</span>
+                </div>
+                <div className="typed-keys"></div>
+                <div className="completed-words">
+                    <ol>
+                        <li>programmed</li>
+                        <li>circuit</li>
+                        <li>javascript</li>
+                    </ol>
+                </div>
+            </Wrapper>
+        </React.Fragment>
     )
 }
 
